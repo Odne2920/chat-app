8c0b7dc0bb51f638f0794c18e6941709730908f6
HyperRush Chat <157821232+hrn-chat@users.noreply.github.com>
2026-03-03 04:22:28 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "hrn-cache-v1.0.01007";
+const CACHE_NAME = "hrn-cache-v1.0.01008";
 const FILES_TO_CACHE = [
-  "./index.html?v=13",
-  "./assets/logic.js?v=13",
-  "./assets/branding/app/icon-192x192-maskable.png?v=13",
-  "./assets/branding/app/icon-192x192-not-maskable.png?v=13",
-  "./assets/branding/app/icon-256x256-maskable.png?v=13",
-  "./assets/branding/app/icon-256x256-not-maskable.png?v=13",
-  "./assets/branding/app/icon-512x512-maskable.png?v=13",
-  "./assets/branding/app/icon-512x512-not-maskable.png?v=13"
+  "./index.html?v=14",
+  "./assets/logic.js?v=14",
+  "./assets/branding/app/icon-192x192-maskable.png?v=14",
+  "./assets/branding/app/icon-192x192-not-maskable.png?v=14",
+  "./assets/branding/app/icon-256x256-maskable.png?v=14",
+  "./assets/branding/app/icon-256x256-not-maskable.png?v=14",
+  "./assets/branding/app/icon-512x512-maskable.png?v=14",
+  "./assets/branding/app/icon-512x512-not-maskable.png?v=14"
 ];
-self.addEventListener("install",(e=>{self.skipWaiting(),e.waitUntil(caches.open(CACHE_NAME).then((e=>e.addAll(FILES_TO_CACHE).catch((()=>{})))))})),self.addEventListener("activate",(e=>{self.clients.claim(),e.waitUntil(caches.keys().then((e=>Promise.all(e.map((e=>e!==CACHE_NAME&&caches.delete(e)))))))})),self.addEventListener("fetch",(e=>{const t=e.request;"navigate"!==t.mode?e.respondWith(caches.match(t).then((e=>e||fetch(t)))):e.respondWith(fetch(t).catch((()=>caches.match("./index.html?v=13"))))}));
+self.addEventListener("install",(e=>{self.skipWaiting(),e.waitUntil(caches.open(CACHE_NAME).then((e=>e.addAll(FILES_TO_CACHE).catch((()=>{})))))})),self.addEventListener("activate",(e=>{self.clients.claim(),e.waitUntil(caches.keys().then((e=>Promise.all(e.map((e=>e!==CACHE_NAME&&caches.delete(e)))))))})),self.addEventListener("fetch",(e=>{const t=e.request;"navigate"!==t.mode?e.respondWith(caches.match(t).then((e=>e||fetch(t)))):e.respondWith(fetch(t).catch((()=>caches.match("./index.html?v=14"))))}));
