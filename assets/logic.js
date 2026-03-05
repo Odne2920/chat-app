37d6136fb2a527a862abee666662847a0008e634
HyperRush Chat <157821232+hrn-chat@users.noreply.github.com>
2026-03-05 01:17:48 +0100

    Update logic.js

--- assets/logic.js
+++ assets/logic.js
@@ -26,7 +26,7 @@ export function initHRNchat(customConfig = {}) {
         requestTimeout: 3000
     };
     const AVATARS = ['./assets/avatars/1.webp', './assets/avatars/2.webp', './assets/avatars/3.webp', './assets/avatars/4.webp', './assets/avatars/5.webp'];
-    const DB_NAME = 'HRN_LOCAL_DB_2';
+    const DB_NAME = 'HRN_LOCAL_DB_5';
     const DB_VERSION = 1;
     const state = {
         user: null,
