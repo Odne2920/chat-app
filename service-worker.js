096f16695899b67f1e701a2728be23f5ede56378
HyperRush Chat <157821232+hrn-chat@users.noreply.github.com>
2026-03-09 23:40:43 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'hrn-cache-1.0.5-3';
-const RUNTIME_CACHE = 'hrn-runtime-cache-1.0.5-3';
+const CACHE_NAME = 'hrn-cache-1.0.5-4';
+const RUNTIME_CACHE = 'hrn-runtime-cache-1.0.5-4';
 
 const SHELL_ASSETS = [
   './',
